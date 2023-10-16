924d861a7aeba7b85d28e90545890f6e4862b2ca
iceeeberg <72841352+iceeeberg@users.noreply.github.com>
2023-10-16 16:04:34 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+const add = document.getElementById("add");
+const add2 = document.getElementById("add2");
+const button = document.getElementById("button");
+
+function performOperation(x, y) {
+  return parseFloat(x.value) + parseFloat(y.value);
+}
+
+button.addEventListener("click", () => {
+  if (add != NaN || add2 != NaN) {
+    let result = document.createElement("p");
+    result.textContent = `Result is: ${performOperation(add, add2)}`;
+    document.body.appendChild(result);
+    add.value = "";
+    add2.value = "";
+  }
+});
